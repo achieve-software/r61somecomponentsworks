86d6bf0f52d37cd0cda38a4acf67d13253ca157b
Achieve-software <vtcbcdtyr@gmail.com>
2023-12-14 11:53:47 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Candle from './components/Candle';
+import Soci from './components/Soci';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Candle/>
+      <br />
+     <Soci/>
+     <br />
+      <Button/>
+      <br />
+    
     </div>
   );
 }
